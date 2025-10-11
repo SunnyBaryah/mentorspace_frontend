4671ed7d1e6d34eded341c14e75389a9d6514f60
SunnyBaryah <sunny.baryah.2602@gmail.com>
2025-10-11 20:47:42 +0530

    updated backend url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const backend_url = "https://mentorspace-backend.vercel.app/api/v1";
-export const socket_url = "https://mentorspace-backend.vercel.app";
+export const backend_url = "https://mentorspace-backend.onrender.com/api/v1";
+export const socket_url = "https://mentorspace-backend.onrender.com";
 
 // export const backend_url = "http://localhost:9000/api/v1";
 // export const socket_url = "http://localhost:9000";
