d043a12c33ada355d4dc5ea6b7531485ee6b1749
SunnyBaryah <sunny.baryah.2602@gmail.com>
2025-10-14 01:57:05 +0530

    change backend to railway

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,10 @@
-export const backend_url = "https://mentorspace-backend.onrender.com/api/v1";
-export const socket_url = "https://mentorspace-backend.onrender.com";
+export const backend_url =
+  "https://mentorspacebackend-production.up.railway.app/api/v1";
+export const socket_url =
+  "https://mentorspacebackend-production.up.railway.app";
+
+// export const backend_url = "https://mentorspace-backend.onrender.com/api/v1";
+// export const socket_url = "https://mentorspace-backend.onrender.com";
 
 // export const backend_url = "http://localhost:9000/api/v1";
 // export const socket_url = "http://localhost:9000";
